2f8f06e27f7d187af71c6015141fde7f87495285
João Treuk <joaotreuk@outlook.com>
2021-06-23 22:52:44 -0300

    -Testes do método SizeOf

---
+++ Unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TForm1 = class(TForm)
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  i: Char;
+begin
+  WriteLn(SizeOf(Word));
+  WriteLn(SizeOf(i));
+end;
+
+end.
